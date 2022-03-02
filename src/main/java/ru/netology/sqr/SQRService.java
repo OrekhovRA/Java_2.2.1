6ca53c22b0d3efec2d7acc536c1f03364de037e4
Orekhov Roman <orekhov-roman44@yandex.ru>
2022-03-02 19:50:32 +0300

    commit 2

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -4,15 +4,54 @@ public class SQRService {
     public int squareNumber(int counter) {
 
         for (int i = 14; i <= 99; i++) {
-            if (i * i >= 200) {
-                if (i * i <= 300) {
+            if (i * i >= 200 && i * i <= 300) {
                     counter++;
                     System.out.println(counter);
                 }
+            }
+        return counter;
+    }
+
+    public int squareNumber1(int counter) {
+
+        for (int i = 14; i <= 99; i++) {
+            if (i * i >= 250 && i * i <= 600) {
+                counter++;
+                System.out.println(counter);
+            }
+        }
+        return counter;
+    }
+
+    public int squareNumber2(int counter) {
+
+        for (int i = 14; i <= 99; i++) {
+            if (i * i >= 3000 && i * i <= 6500) {
+                counter++;
+                System.out.println(counter);
+            }
+        }
+        return counter;
+    }
+
+    public int squareNumber3(int counter) {
+        for (int i = 14; i <= 99; i++) {
+            if (i * i >= 9000 && i * i <= 10000) {
+                counter++;
+                System.out.println(counter);
+            }
+        }
+        return counter;
+    }
+
+    public int squareNumber4(int counter) {
+        for (int i = 14; i <= 99; i++) {
+            if (i * i >= 9500 && i * i <= 11000) {
+                counter++;
+                System.out.println(counter);
             }
         }
         return counter;
     }
 }
-
 
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -15,5 +15,45 @@ class SQRServiceTest {
         int actual = service.squareNumber(num);
         assertEquals(expected, actual);
     }
+
+    @Test
+    void testSquareNumber1() {
+        SQRService service = new SQRService();
+        int num = 0;
+        int expected = 9;
+
+        int actual = service.squareNumber1(num);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testSquareNumber2() {
+        SQRService service = new SQRService();
+        int num = 0;
+        int expected = 26;
+
+        int actual = service.squareNumber2(num);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testSquareNumber3() {
+        SQRService service = new SQRService();
+        int num = 0;
+        int expected = 5;
+
+        int actual = service.squareNumber3(num);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testSquareNumber4() {
+        SQRService service = new SQRService();
+        int num = 0;
+        int expected = 2;
+
+        int actual = service.squareNumber4(num);
+        assertEquals(expected, actual);
+    }
 }
 
